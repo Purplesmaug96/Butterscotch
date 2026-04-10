8de4e5b78dccc5e452a73266b04ef2a87b6c2b20
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-04-10 14:42:04 +0800

    Fix

--- xbcomp.sh
+++ xbcomp.sh
@@ -2,7 +2,7 @@ set -e
 
 rm -rf build-xbox
 
-export NXDK_DIR=$(pwd)/nxdk
+export NXDK_DIR=$(pwd)/vendor/nxdk
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$NXDK_DIR/share/toolchain-nxdk.cmake -DPLATFORM=xbox -B build-xbox -DNXDK_DIR=$NXDK_DIR .
 
@@ -17,4 +17,4 @@ mkdir -p iso_root
 cp default.xbe iso_root/
 cp -r ../undertale/* iso_root/
 
-/hdd/Butterscotch/nxdk/tools/extract-xiso/extract-xiso -c iso_root/ butterscotch.iso
+$NXDK_DIR/tools/extract-xiso/extract-xiso -c iso_root/ butterscotch.iso
