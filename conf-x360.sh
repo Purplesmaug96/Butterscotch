1a4ea938a9ede1e7ca010e1db9103b0ca73603f3
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-07-11 13:02:42 +0800

    Configures for the 360 yay!!!!!

---
+++ conf-x360.sh
@@ -0,0 +1,1 @@
+cmake -DPLATFORM=xbox360-xdk -DWERROR=ON -DAUDIO_BACKEND=xaudio2 -DENABLE_ANGLE=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_TOOLCHAIN_FILE="vendor/cmake-xenon-toolchain/cmake/toolchain/xenon-toolchain.cmake" -B build-x360-xdk
